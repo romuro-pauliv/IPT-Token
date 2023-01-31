68b74ca799947041c59d453581c31126085f88db
Romulo Pauliv <romulopauliv@bk.ru>
2023-01-31 09:55:20 -0300

    remove wrong dist

--- package/build/lib/iptoken/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-IPT-Token.
-Decorator system for generating and requiring access tokens for your routes in your Flask application.
-"""
-
-__version__ = "0.9.1"
-__author__ = 'Rômulo Pauliv'
-
-from .module_iptoken import IPToken
